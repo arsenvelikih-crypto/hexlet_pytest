252c44c3fd942e6d58b31cceeee19fec0b5070f1
arsenvelikih-crypto <arsenvelikih@gmail.com>
2026-02-11 21:58:43 +0400

    add stack

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,3 @@
 def reverse(string):
     return string[::-1]
+
---
+++ tests/test_stack.py
@@ -0,0 +1,28 @@
+import pytest
+from hexlet_pytest import stack
+
+
+def test_stack():
+    stack = []
+    stack.append("one")
+    stack.append("two")
+
+    assert stack.pop() == "two"
+    assert stack.pop() == "one"
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append("one")
+    assert bool(stack)  # not not stack
+
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    # проверить что вызывается конкретное исключение можно с помощью конструкции with pytest.raises()
+    # если внутри блока вызовется исключение, то тест будет пройден
+    with pytest.raises(IndexError):
+        stack.pop()
