5b361060d90c6e0632657154238459654b790879
arsenvelikih-crypto <arsenvelikih@gmail.com>
2026-02-14 16:24:50 +0400

    update files

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,17 @@
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
     return string[::-1]
 
+
+# Нужно для запуска тестов
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
